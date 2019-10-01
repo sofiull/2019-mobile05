4936627f4ed13721c1972f118595aa5378b6d8a8
sofiull <sofiulfr@gmail.com>
2019-10-01 16:24:20 +0700

    Menyelesaikan Transition Fragment

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -1,10 +1,15 @@
 package polinema.ac.id.starterchapter05.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import polinema.ac.id.starterchapter05.R;
 import polinema.ac.id.starterchapter05.fragments.BlueFragment;
@@ -19,19 +24,39 @@ public class DynamicActivity extends AppCompatActivity {
     }
 
     public void handlerClickLoadRedFragment(View view) {
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
-        fragmentTransaction.replace(R.id.frameout, new RedFragment(),"RED_FRAGMENT");
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+        //   Todo: Mengecek apakah sudah terload atau belum
+        //         Jika sudah maka tidak perlu diload kembali
+
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag("RED_FRAGMENT");
+        if(fragment == null || checkCurrent().equals("BlueFragment")){
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_from_left, R.anim.enter_from_left, R.anim.exit_from_right);
+            fragmentTransaction.replace(R.id.frameout, new RedFragment(), "RED_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }else{
+            Toast.makeText(this, "Blue Fragment Telah Dimuat Sebelumnya", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void handlerClickLoadBlueFragment(View view) {
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        fragmentTransaction.replace(R.id.frameout, new BlueFragment(),"BLUE_FRAGMENT");
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+        //   Todo: Mengecek apakah sudah terload atau belum
+        //         Jika sudah maka tidak perlu diload kembali
+
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag("BLUE_FRAGMENT");
+        if (fragment == null || checkCurrent().equals("RedFragment")) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            fragmentTransaction.replace(R.id.frameout, new BlueFragment(), "BLUE_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }else{
+            Toast.makeText(this, "Blue Fragment Telah Dimuat Sebelumnya", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public String checkCurrent(){
+        return getSupportFragmentManager().findFragmentById(R.id.frameout).getClass().getSimpleName();
     }
 }
 
